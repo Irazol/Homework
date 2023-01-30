6b5a8eb542eb5d7c15dd990c5270e8d42a4bc3e3
Іра <hho-h@ukr.net>
2023-01-30 21:44:48 +0200

    )

--- src/Lesson17/ex_004_comparable/Main.java
+++ src/Lesson17/ex_004_comparable/Main.java
@@ -1,7 +1,7 @@
 package Lesson17.ex_004_comparable;
 
 import java.util.Arrays;
-import java.util.Comparator;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -9,7 +9,8 @@ public class Main {
         Car c2 = new Car(190, 2000, "Opel", "Black");
         Car c3 = new Car(185, 10000, "Mercedes", "White");
         Car c4 = new Car(200, 15000, "Porsche", "Green");
-        Car[] c = {c1, c2, c3,c4};
+        Car c5 = new Car(180, 12000, "Ford", "Pink");
+        Car[] c = {c1, c2, c3, c4, c5};
 
         Arrays.sort(c);
         System.out.println("Сортування за швидкістю");
@@ -17,9 +18,10 @@ public class Main {
             System.out.println(temp);
         }
         c1.speed = 100;
-        c2.speed= 100;
+        c2.speed = 100;
         c3.speed = 100;
         c4.speed = 100;
+        c5.speed = 100;
 
         Arrays.sort(c);
         System.out.println("\nСортування за ціною");
@@ -27,9 +29,10 @@ public class Main {
             System.out.println(temp);
         }
 
-        c2.price= 8000;
+        c2.price = 8000;
         c3.price = 8000;
         c4.price = 8000;
+        c5.price = 8000;
 
         Arrays.sort(c,new Car());
         System.out.println("\nСортування за моделю");
@@ -40,6 +43,7 @@ public class Main {
         c2.model= "BMW";
         c3.model= "BMW";
         c4.model= "BMW";
+        c5.model= "BMW";
 
         Arrays.sort(c,new Car());
         System.out.println("\nСортування за коляром");
